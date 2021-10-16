7337194dcb96c62ddcf618f1942781e0e74af632
CharlieJhonSmith <charlie.alfrido@gmail.com>
2021-10-16 14:35:55 +0000

    add test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,35 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react"
+import { render } from "@testing-library/react"
+import App from "./App"
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+// @ponicode
+describe("updateAuthorsList", () => {
+    let inst
+
+    beforeEach(() => {
+        inst = new App.default()
+    })
+
+    test("0", async () => {
+        await inst.updateAuthorsList()
+    })
+})
+
+// @ponicode
+describe("componentDidMount", () => {
+    let inst
+
+    beforeEach(() => {
+        inst = new App.default()
+    })
+
+    test("0", async () => {
+        await inst.componentDidMount()
+    })
+})
